1c7f8db99f2b8ae0ed62dff9a062f897807b50e9
Dave Warnock <dwarnock@gmail.com>
2024-01-19 12:39:58 +0000

    Initial load database url from .env with tests Currently initialize just prints a message.

--- examples/sqlite/src/main.rs
+++ examples/sqlite/src/main.rs
@@ -1,3 +1,28 @@
+use dotenvy::dotenv;
+use axum_tenancy;
+
 fn main() {
     println!("Hello, world!");
+    dotenv().expect(".env file not found");
+    axum_tenancy::initialize();
+}
+
+
+#[cfg(test)]
+mod tests {
+    use std::env;
+    use dotenvy::dotenv;
+
+    #[test]
+    fn sqlite_database_url_correct() {
+        dotenv().expect(".env file not found");
+
+        let database_url: String = env::var("DATABASE_URL").expect(".env missing DATABASE_URL");
+        assert_eq!(database_url, "sqlite:axum-tenancy.sqlite?mode=rwc".to_string());
+    }
+
+    #[test]
+    fn axum_tenancy_initialise_ok() {
+        axum_tenancy::initialize(); // would panic if it fails
+    }
 }
