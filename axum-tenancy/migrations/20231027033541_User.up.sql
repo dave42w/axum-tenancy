9da0867945d3b2fb4da3a04ed355a3620e52bf64
Dave Warnock <dwarnock@gmail.com>
2024-01-20 20:44:11 +0000

    First version switched to Postgresql (kept having table not fo9und errors trying to use sqlx::query! in tests). DATABASE_URL now includes password so using bash export variable for that.

--- axum-tenancy/migrations/20231027033541_User.up.sql
+++ axum-tenancy/migrations/20231027033541_User.up.sql
@@ -1,19 +1,15 @@
-CREATE TABLE IF NOT EXISTS User (
-    id TEXT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS AppUser (
+    id uuid PRIMARY KEY,
     user_name TEXT NOT NULL,
     hash_password TEXT NOT NULL,
     display_name TEXT NOT NULL,
-    is_admin INTEGER NOT NULL,
+    is_admin BOOLEAN NOT NULL,
     email TEXT NOT NULL,
     mobile_phone TEXT NOT NULL,
     UNIQUE (user_name),
     UNIQUE (display_name)
-) STRICT, WITHOUT ROWID;
-
-CREATE INDEX IF NOT EXISTS idxUserEmail ON User (
-    email
 );
 
-CREATE INDEX IF NOT EXISTS idxUserMobilePhone ON User (
-    mobile_phone
-);
+CREATE INDEX IF NOT EXISTS idxUserEmail ON AppUser (email);
+
+CREATE INDEX IF NOT EXISTS idxUserMobilePhone ON AppUser (mobile_phone);
--- axum-tenancy/src/lib.rs
+++ axum-tenancy/src/lib.rs
@@ -1,13 +1,44 @@
+use sqlx;
+
 pub fn initialize() {
     println!("Initialize axum-tenancy");
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
+use sqlx::postgres::PgPool;
 
-    #[test]
-    fn initialize_without_panic() {
-        initialize();
-    }
+#[test]
+fn initialize_without_panic() {
+    initialize();
 }
+
+#[sqlx::test]
+async fn add_appuser(pool: PgPool) -> sqlx::Result<()> {
+    sqlx::query!(
+        r#"
+        INSERT INTO AppUser (
+            id, 
+            user_name, 
+            hash_password, 
+            display_name, 
+            is_admin, 
+            email,
+            mobile_phone
+        )
+        VALUES (
+            'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 
+            'Dave42W',
+            '0',
+            'Dave Warnock',
+            TRUE,
+            'dwarnock@gmail.com',
+            '07886 553376'
+        )
+        "#
+    )
+    .execute(&pool)
+    .await?;
+    Ok(())
+    
+}
+
