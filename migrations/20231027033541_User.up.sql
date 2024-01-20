29f2b03694b4be504573b05dd96b8313a8f42c38
Dave Warnock <dwarnock@gmail.com>
2024-01-20 13:02:17 +0000

    Starting to add sqlx for user table

---
+++ migrations/20231027033541_User.up.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS User (
+    id TEXT PRIMARY KEY,
+    user_name TEXT NOT NULL,
+    hash_password TEXT NOT NULL,
+    display_name TEXT NOT NULL,
+    is_admin INTEGER NOT NULL,
+    email TEXT NOT NULL,
+    mobile_phone TEXT NOT NULL,
+    UNIQUE (user_name),
+    UNIQUE (display_name)
+) STRICT, WITHOUT ROWID;
+
+CREATE INDEX IF NOT EXISTS idxUserEmail ON User (
+    email
+);
+
+CREATE INDEX IF NOT EXISTS idxUserMobilePhone ON User (
+    mobile_phone
+);
